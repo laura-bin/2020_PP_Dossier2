ec8d2da2a1d533ae338c5ee84770763e78733102
Laura Bin <l.binacchi@gmail.com>
2021-04-01 23:22:29 +0200

    rewrite copy test

--- tests/test_copy.c
+++ tests/test_copy.c
@@ -21,33 +21,34 @@ int failures = 0;
 
 // diff test_copy_out/tested.bmp ../images/tested.bmp
 // cmp -l test_copy_out/tested.bmp ../images/tested.bmp | gawk '{printf "%08X %02X %02X\n", $1, strtonum(0$2), strtonum(0$3)}'
-void test(char **tests, unsigned n) {
+void test(char *tested) {
     bmp3_image source;
-    unsigned i;
 
     puts("");
 
-    for (i = 0; i < n; i++) {
-        // load the image from the source directory
-        if (load_bmp3(tests[i], ".." DIR_SEP SOURCE_DIR, &source)) {
-            failures++;
-            continue;
-        }
-
-        // write the image in the destination directory
-        if (write_bmp3(&source, DEST_DIR)) {
-            failures++;
-        }
-        free_bmp3(&source);
+    // load the image from the source directory
+    if (load_bmp3(tested, ".." DIR_SEP SOURCE_DIR, &source)) {
+        failures++;
+        return;
     }
+
+    // write the image in the destination directory
+    if (write_bmp3(&source, DEST_DIR)) {
+        failures++;
+    }
+    free_bmp3(&source);
 }
 
 int main (void) {
 
     make_sub_dir(DEST_DIR);
 
-    test((char*[]){ "detmer", "georgiou", "burnham_spock",
-            "landscape1", "landscape2", "landscape3"}, 6);
+    test("detmer");
+    test("georgiou");
+    test("burnham_spock");
+    test("landscape1");
+    test("landscape2");
+    test("landscape3");
 
     return failures;
 }
