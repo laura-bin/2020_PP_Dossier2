27956eedd35396841768cd7020db4a7add50e922
Laura Bin <l.binacchi@gmail.com>
2021-04-02 10:24:39 +0200

    remove redundant image name

--- include/mosaic_filter.h
+++ include/mosaic_filter.h
@@ -14,7 +14,6 @@
  * 
  * @param source: source image to reduce
  * @param dest: destination image (reduced image)
- * @param det_name: name of the destination image
  * @param dest_width: width of the reduced image
  * @param dest_height: height of the reduced image
  * @param factor: reduction factor
@@ -23,4 +22,4 @@
  *      0 if the image has successfully been reduced
  *     -1 if a parameter is invalid or if an error occured
  */
-int reduce(bmp3_image *source, bmp3_image *dest, char *dest_name, int dest_width, int dest_height, int factor);
+int reduce(bmp3_image *source, bmp3_image *dest, int dest_width, int dest_height, int factor);
